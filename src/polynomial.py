5c6d277e646969d91f336499906ed1a8b51e8021
Aliaksei Sedzin <sedzin@list.ru>
2018-03-27 11:57:08 +0200

    First fully functional version

---
+++ src/polynomial.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 25 20:26:51 2018
+
+@author: Alex
+"""
+import math
+
+class polynomial():
+    
+    def __init__(self,coef=[0]):
+        self.degree=len(coef)-1
+        self.coef=coef
+        self.findDegree()
+        
+    def iszero(self):
+        for i in range(self.degree+1):
+            if self.coef[i]!=0:
+                return False
+        return True 
+    
+    def findDegree(self):
+        d=len(self.coef)-1
+        while d>0 and self.coef[d]==0:
+            d-=1
+        self.coef=self.coef[0:d+1]
+        self.degree=d   
+        
+    def add(self,v):
+        degree=max(v.degree,self.degree)
+        coef=[0]*(degree+1)
+        for i in range(v.degree+1):
+            coef[i]+=v.coef[i]
+        for i in range(self.degree+1):
+            coef[i]+=self.coef[i]
+        return polynomial(coef)
+
+    def sub(self,v):
+        return self.add(v.umin())
+    
+    def mul(self,v):
+        coef=[0]*(v.degree+self.degree+1)
+        for i in range(v.degree+1):
+            for j in range(self.degree+1):
+                coef[i+j]+=v.coef[i]*self.coef[j]
+        return polynomial(coef)
+        
+    def div(self,d):    
+        if d.iszero():
+            raise Exception('Division by 0')    
+        q=polynomial()
+        r=self    
+        while not r.iszero() and r.degree>=d.degree :
+            coef=[0]*(r.degree-d.degree+1)
+            coef[-1]=r.coef[r.degree]/d.coef[d.degree]
+            t=polynomial(coef)
+            q=q.add(t)
+            r=r.sub(t.mul(d))
+        if not r.iszero():
+            raise Exception('Non-zero reminder while dividing polynomials')
+        return q
+    
+    def umin(self):
+        coef=[0]*(self.degree+1)
+        for i in range(self.degree+1):
+            coef[i]=-self.coef[i]
+        return polynomial(coef)
+   
+    def log(self):
+        if self.degree>0:
+            raise Exception('Can not take log from a polinome')
+        if self.coef[0]<=0:
+            raise Exception('Log from a non-positive argument')
+        coef=math.log(self.coef[0])
+        return polynomial([coef])
+    
+    
+    def solve(self):
+        if self.degree>1:
+            if self.coef[0]==0:
+                return('0')
+            return ('Not found (not able to solve non-linear equations)')
+        
+        if (self.degree==0 and self.coef[0]==0):
+            return 'Any (any value is a valid solution)'
+        if self.degree==0 and self.coef[0]!=0:
+            return 'None (no solution exists)'
+        
+        
+        return polynomial([-self.coef[0]/self.coef[1]])
+    
+    
+    
+    def string(self):
+        s=''
+        sign=''
+    
+        if self.degree>=0:
+        
+        # 
+            for i in range(self.degree+1):
+                c=round(self.coef[i],9)
+                if c==int(c):
+                    c=int(c)
+                if c!=0 or self.degree==0:
+                    s=sign+s
+                    
+                    if i==1:
+                        s='x'+s
+                    elif i>1: 
+                        s='x^'+str(i)+s    
+                    
+                    a=abs(c)
+                    if i==0 or a!=1: 
+                        s=str(a)+s
+    
+                    
+                    if c<0:
+                        sign='-'
+                    else: 
+                        sign='+'
+                
+            if sign=='-':
+                s='-'+s
+        return s
+
+    def __str__(self):
+        return self.string()    
+    
+    def __repr__(self):
+        return self.string()    
+    
+        
--- src/scalc.py
+++ src/scalc.py
@@ -5,35 +5,72 @@ Created on Thu Mar 22 15:08:24 2018
 @author: Alex
 """
 import sys
-import math
+from polynomial import polynomial
 
 class Error(Exception):
     def __init__(self,message,position):
         self.message = message
         self.position = position
+        
 
+# Classify and sanity check the sting (allowed characters, equation or expression, variables)
+# Then process the string according to the classification
+def assess(string):
+    
+    EqualSignPos=-1
+    Variable='#'
+    
+    for i in range(0,len(string)):
+        if string[i] not in ' 01234567890.-+*/()logxy=':
+            raise Error('Unrecognised character "'+string[i]+'"',i)
+        if string[i] == '=':
+            if EqualSignPos>=0:
+                raise Error('An extra "=" character found',i)
+            EqualSignPos=i
+        if string[i] in 'xy':
+            if Variable in 'xy' and string[i]!=Variable:
+                raise Error('Seen variable "'+Variable+'" already, but an extra variable "'+string[i]+'" found',i)
+            Variable=string[i]
+        
+    
+    if EqualSignPos<0: # expression; evaluate it
+        r=evaluate(string,0)
+        r=str(r)
+        r=r.replace('x',Variable)
+        return r
+        
+    if EqualSignPos>=0: # equation; try to solve it
+        
+        p1=evaluate(string[0:EqualSignPos],0) # evaluate the left side
+        p2=evaluate(string[EqualSignPos+1:],EqualSignPos+1) # evaluate the right side
+        p3=p1.sub(p2) # subtract the latter from the former
+        
+        if Variable!='#': # equation with a variable; solve it
+            return Variable+'='+str(p3.solve())
+        else: # no variables; check if the equation is an identity
+            if str(p3)=='0':
+                return 'True'
+            else:
+                return 'False'
 
-# TODO: correctly process expressions like 2 3
-
+# Evaluate an expression
 def evaluate(string,stringPosition):
     # strip leading and trailing spaces
     newString=string.strip()
     stringPosition+=string.find(newString)
     string=newString
-    
-    
+     
     if len(string)==0:
         raise Error('Expression expected',stringPosition)
-
     
-      
     # make a table containing positions of operators outside brackets
-    operators=[-1]*8
+    # following are the operators in increasing order of priority
+    # [+, -, *, /, implicit *, log, unary +, unary - ]
+    opPositions=[-1]*8
     openBrackets=0
     lastSymbol='#'
     for i in range(0,len(string)):
-        if string[i] not in ' 01234567890.-+*/()logxy':
-            raise Error('Unrecognised character "'+string[i]+'"',stringPosition+i)
+
         if string[i]=='(':
             openBrackets+=1
         if string[i]==')':
@@ -42,45 +79,43 @@ def evaluate(string,stringPosition):
             raise Error('Unexpected closing bracket', stringPosition+i)
         if openBrackets==0: # only evaluate expressions outside brackets
             if string[i]=='+' and i>0: 
-                operators[0]=i            
+                opPositions[0]=i            
             if string[i]=='-' and i>0:
-                operators[1]=i
+                opPositions[1]=i
             if string[i]=='*': 
-                operators[2]=i
+                opPositions[2]=i
             if string[i]=='/': 
-                operators[3]=i
+                opPositions[3]=i
         # handle implicit multiplication
         if openBrackets==0 or (openBrackets==1 and string[i]=='(' ) :
             if  ( (lastSymbol in '0123456789)' and string[i] in '(xyl') or 
                  (lastSymbol in 'xy' and string[i] in '(')  or
                  (lastSymbol in ')' and string[i] in '01234567890.') ):
-                operators[4]=i
+                opPositions[4]=i
                 
         lastSymbol=string[i]
     if openBrackets>0:
-        raise Error('Expecting closing bracket -',stringPosition+i+1)
+        raise Error('Expecting closing bracket',stringPosition+i+1)
     
     if len(string)>2 and string[0:3]=='log':
         if len(string)<4 or string[3] !='(':
             raise Error('Expecting opening bracket of log function',stringPosition+3)
-        operators[5]=2
+        opPositions[5]=2
     if string[0] == '+':
-        operators[6]=0
+        opPositions[6]=0
     if string[0] == '-':
-        operators[7]=0
+        opPositions[7]=0
     
-    # find the operator with least priority 
+    # find the operator with the least priority 
     operator=-1
     for j in reversed(range(8)):
-        if operators[j]>=0:
+        if opPositions[j]>=0:
             operator=j
-    if operator in range(2,5): # need to take the right-most multiplication
-        operator = operators.index(max(operators[2:5]))
+    if operator in range(2,5): # need to take the right-most multiplication or division
+        operator = opPositions.index(max(opPositions[2:5]))
     
-    pos=operators[operator] #position of the operator
-    
-    if operator>=0: # 
-       
+    pos=opPositions[operator] #position of the operator with least priority
+    if operator>=0: 
         # form and evaluate argument strings
         if operator in range(0,4): # +-*/
             s1=string[0:pos]
@@ -99,34 +134,37 @@ def evaluate(string,stringPosition):
             s2=string[pos+1:]
             r2 = evaluate(s2,stringPosition+pos+1)
         
-        # process the results of evaluation
+        # apply the operator
 
         if operator==0 : # +
-            result = r1+r2
-        if operator==1 : #
-            result = r1-r2
+            result = r1.add(r2)
+        if operator==1 : # -
+            result = r1.sub(r2)
         if operator==2 or operator==4: # * or implicit *
-            result = r1*r2
+            result = r1.mul(r2)
             
-        if operator==3 : #
-            
-            if r2 == 0:
-                raise Error('Division by 0',stringPosition+pos)
-            result = r1/r2
+        if operator==3 : # /
+            try:
+                result = r1.div(r2)
+            except Exception as e:
+                raise Error(e,stringPosition+pos)
         
         if operator==5 : # log
-            if r2 <= 0:
-                raise Error('Log from a non-positive argument',stringPosition+4)
-            result = math.log(r2)
+            try:
+                result = r2.log()
+            except Exception as e:
+                raise Error(e,stringPosition+4)
             
-        if operator==6 : #
+        if operator==6 : # unary +
             result = r2
-        if operator==7 : #
-            result = -r2
+        if operator==7 : # unary -
+            result = r2.umin()
         
         return result
     
-    # try to unfold the brackets
+    # no operators found outside of brackets
+    
+    # unfold the brackets
     if string[0]=='(':
         if string[-1]==')':
             s2=string[1:-1]
@@ -135,10 +173,13 @@ def evaluate(string,stringPosition):
         else:
             raise Error('Expecting closing bracket',stringPosition+len(string))
     
-    # try to find variables
+    # parse variables
     if string[0] in 'xy':
-            raise Error('Not (yet) able to process a variable',stringPosition)
+        if len(string)>1:
+            raise Error('Unexpected character after a variable "'+string[0]+'"',stringPosition+1)
+        return polynomial([0,1])
     
+    # parse numbers
     decimalFound=False
     for i in range(len(string)):
         if string[i] not in '0123456789.':
@@ -146,17 +187,22 @@ def evaluate(string,stringPosition):
         if string[i]=='.':
             if decimalFound==True:
                 raise Error('Unexpected (extra) decimal point',stringPosition+i)
-    try :
-        return float(string)
-    except Exception as e:
-        raise Error('Could not evaluate expression',stringPosition)
+    
+    return polynomial([float(string)])
+    
+    # something went wrong during parsing
+    raise Error('Could not evaluate expression',stringPosition)
     
     
 def helpText():
     value=(
            'Simple Scientific Calculator\n'+
            'USAGE:    scalc [math expression]\n'+
-           'Example:  scalc (2+3)*6' )
+           'Supported functions: +,-,*,/,log\n'+
+           'Supported variables: x,y\n' +
+           'Supported equations type: linear\n' +
+           'Examples:  scalc (2+3)*6, scalc x+10=12-x, scalc "y(y+y)y*log(10)"\n'
+           )
     return value
 
 
@@ -166,28 +212,26 @@ def scalc():
         args = sys.argv
         narg=len(args)
         if narg==1:
-            value=helpText()
+            output=helpText()
         elif narg>2:
             raise Error('Error: Too many command line arguments',-1)
         else:
-            r = evaluate(args[1],0)
-            r = round(r,9)
-            value=str(r)
+            output = assess(args[1])
     except Error as e:
         #raise e # uncomment the line if need to debug the code
         if e.position<0:
-            value = str(e.message)
+            output = str(e.message)
         else:
             space=' '*e.position
-            value = ( args[1]+'\n'+
+            output = ( args[1]+'\n'+
                       space+'^\n'+
-                      'Error: '+ str(e.message)+' at positoin '+str(e.position+1) )
+                      'Error: '+ str(e.message)+' at position '+str(e.position+1) )
                       
             
-    return (value)
+    return output
 
 if __name__=='__main__':
-    value=scalc()
-    print(value)
+    output=scalc()
+    print(output)
 
     
